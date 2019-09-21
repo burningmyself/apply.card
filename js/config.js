7082167650687c4ce46ff300bb25773b1cd4e8f1
yangfubing <1928881525@qq.com>
2019-09-21 15:57:31 +0800

    add cardlist
    Signed-off-by: yangfubing <1928881525@qq.com>

---
+++ js/config.js
@@ -0,0 +1,47 @@
+const cardList = [
+  {
+    "url": "https://ecentre.spdbccc.com.cn/creditcard/indexActivity.htm?data=F1806281013015337503",
+    "name":"浦发银行",
+    "logo":"assets/pf.png"
+  },
+  {
+    "url": "https://creditcard.ecitic.com/m/tjbk/card_list.html?uid=TJ48787789&amp;sid=SJDKTJBK",
+    "name":"中信银行",
+    "logo":"assets/zx.png"
+  },
+  {
+    "url": "https://b.pingan.com.cn/creditcard/huodong/MGM4ZJ/M2.shtml?source=kdapp&amp;outerSource=mgm4-201712&amp;channel_source=04&amp;usertagid=20280&amp;xykRandomStr=3730020172&amp;rec_no=&amp;rec_type=&amp;media_source=&amp;activity_Id=V0978&amp;activity_FlowId=m_T0MRDFK0nQlCimv14348&amp;SSLSOURCE=BROP-CMP",
+    "name":"平安银行",
+    "logo":"assets/pa.png"
+  },
+  {
+    "url": "https://creditcardapp.bankcomm.com/applynew/front/apply/mgm/account/wechatEntry.html?recomId=7063159&amp;saleCode=&amp;entryRecomId=null&amp;trackCode=A080714237826&amp;isNewCardMgm=2",
+    "name":"交通银行",
+    "logo":"assets/jt.png"
+  },
+  {
+    "url": "https://creditcard.cmbc.com.cn/wsv2/?etr=f8TRGNHmS1LiAEEwKbe3JGw6CA5pmNV2y8QwaPCIPeAv1dVS8bEksPtk7+KFFK4vrzvZghsMOYjStY4iCduNMilBWZiWfBNE95A4/7hN9Ec70u74Ph38LuAguD9ehfyXmd3BRcDmeb3r0KmTM2F0Vj7NmxHc0O9uqpPd9TXUeBkIWjLdQ45N+WGVtcxe4t9M8olE2ql+Vu7/cON33YNY9hZKXN8ltXGC5r/RPMCz8kMytN5DMPzYHyY+GN6sGug38Fm/lNZ3vRFIuIA2Umc0zo7BIcM7kETgHwfTvASzzUPAHHpshH855j9XW8iIOTle89RFqlkBYU81Ufmi/F1JSrHUIlntLmn5abtdiPo6f26iGqTsPM2+BvyZHsryGwuiu99q9+3LoMYTUzTG1dLbXxLeJom1Mh+yL7oR5NoiTb5XoV+Xl4Iwm8rhWrUycr43BHEhxybIpxuZm2HkkCpwXRo6Jocx4nKJI5x8xvti3FHjfAWgvaHkjYOj+5iA+4U2",
+    "name":"民生银行",
+    "logo":"assets/ms.png"
+  },
+  {
+    "url": "https://mims.icbc.com.cn/IMServiceServer/servlet/ICBCBaseReqNSServlet?dse_operationName=ApplyCreditCardOp&amp;coreCode=HZDW000004461&amp;paraPromoCode=TJ0001qglehkzg1b0000&amp;areacode=03100",
+    "name":"工商银行",
+    "logo":"assets/gs.png"
+  },
+  {
+    "url": "https://xyk.cebbank.com/cebmms/apply/psjk/list.htm?pro_code=FHZY010000SZ0001CCTC&amp;c2c_recom_flag=89135537",
+    "name":"光大银行",
+    "logo":"assets/gd.png"
+  },
+  {
+    "url": "https://wm.cib.com.cn/html/webapp/fast-issue/parnter.html?id=b512efa695e5466fa0243972000ce6ab",
+    "name":"兴业银行",
+    "logo":"assets/xy.png"
+  },
+  {
+    "url": "https://apl.cc.hrbb.com.cn/bankcoas/mb/?mobphnNo=CD36CD66E1DE7699338B59A0CF2F151F&amp;Merchant=CF18EB490DE8A2F8&amp;Channel=C981FED17585F2AE&amp;idNum=EAAD24A9F3E01D40FDA688EC6B0F6F63BA733F22F1C0AC88",
+    "name":"哈尔滨银行",
+    "logo":"assets/he.png"
+  },
+];
--- src/js/config.js
+++
@@ -1,6 +0,0 @@
-const cardList=[
-  {"bf":"https://ecentre.spdbccc.com.cn/creditcard/indexActivity.htm?data=F1806281013015337503"},
-  
-]
-
-// export default cardList;https://ecentre.spdbccc.com.cn/creditcard/indexActivity.htm?data=F1806281013015337503
